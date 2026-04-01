d4e7bda7125c4fa3cb942a03e663a9f999864c2a
Bisworanjan Jena <athaljen@gmail.com>
2026-04-02 00:26:06 +0530

    added resume

---
+++ pages/resume.tsx
@@ -0,0 +1,290 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div className="container">
+      {/*  MARK:- PAGE-1 */}
+      <div className="page">
+        {/*  MARK:- Header */}
+        <div className="header">
+          <h1>BISWORANJAN JENA</h1>
+          <div className="sub-header">
+            Software Engineer (React Native | Full Stack)
+            <br />
+            Bhubaneswar, Odisha
+            <br />
+          </div>
+          <div className="sub-header-links">
+            <a href="tel:+916370584925">+91 63705 84925</a>|
+            <a href="mailto:bisworanjanjena23@gmail.com">
+              bisworanjanjena23@gmail.com
+            </a>
+            |
+            <a href="https://github.com/athaljen" target="_blank">
+              GitHub
+            </a>
+            |
+            <a
+              href="https://www.linkedin.com/in/bisworanjanjena23"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
+        {/*  MARK:- Summary */}
+        <h2>SUMMARY</h2>
+        <p>
+          Software Development Engineer with 3+ years of experience building
+          scalable mobile, web, and backend applications using React Native,
+          Next.js, Node.js, Supabase, and Firebase. Demonstrated ability to
+          improve performance, strengthen security, and enhance user experience.
+        </p>
+        {/*  Software Development Engineer with 3+ years of experience specializing */}
+        in React Native, building scalable mobile applications used by 100K+
+        users. Strong expertise in performance optimization, real-time systems,
+        and end-to-end app development across mobile, web, and backend using
+        React Native, Next.js, and Node.js.
+        {/*  MARK:- Achievements */}
+        <h2>ACHIEVEMENTS</h2>
+        <ul>
+          <li>
+            Built and delivered 20+ mobile and web applications across iOS,
+            Android, and web.
+          </li>
+          <li>
+            Achieved 100K+ downloads and 4.1+ average ratings across multiple
+            apps.
+          </li>
+          <li>
+            Developed real-time and AI-powered features improving engagement by
+            and retention.
+          </li>
+          <li>
+            Experience in CI/CD pipelines, scalable architecture, and mentoring
+            developers.
+          </li>
+        </ul>
+        {/*  MARK:- Work Experience */}
+        <h2>WORK EXPERIENCE</h2>
+        <div className="header-justify">
+          <p>
+            <strong>Software Development Engineer I</strong>
+            <br />
+            Xam Guide Private Limited
+          </p>
+          <p className="place-date-box">
+            (March 2025 - Present)
+            <br />
+            Coimbatore, Tamil Nadu (Remote)
+          </p>
+        </div>
+        <ul>
+          <li>
+            Developed and maintained scalable features using Expo, React Native,
+            Supabase, Node.js and backend services improved application
+            performance and user experience.
+          </li>
+          <li>
+            Established CI/CD pipelines using Bitbucket, Docker, and automated
+            testing with Jest and Detox, reducing deployment time and improved
+            code quality.
+          </li>
+          <li>
+            Optimized API calls and state management, reducing load times by 20%
+            and improving app responsiveness
+          </li>
+          <li>
+            Collaborated with cross-functional teams (design, QA) to deliver
+            features on time in an agile environment.
+          </li>
+          <li>
+            Mentored junior developers by conducting code reviews and sharing
+            best practices.
+          </li>
+        </ul>
+        <div className="header-justify" style={{ marginTop: "20px" }}>
+          <p>
+            <strong>React Native Developer </strong>
+            <br />
+            SilverSky Technology
+          </p>
+          <p className="place-date-box">
+            (February 2023 - January 2025)
+            <br />
+            Ahmedabad, Gujarat
+          </p>
+        </div>
+        <ul>
+          <li>
+            Successfully developed and deployed over 15 mobile applications
+            across Play Store and App Store.
+          </li>
+          <li>
+            Utilized RESTful APIs, Web-Sockets, and third-party libraries to
+            enhance functionality and user experience.
+          </li>
+          <li>
+            Collaborated with product managers, cross-functional teams, and QA
+            to deliver high-quality projects on time.
+          </li>
+          <li>
+            Conducted regular code reviews, ensuring adherence to best practices
+            and boosting team productivity.
+          </li>
+        </ul>
+        {/*  MARK:- Skill */}
+        <h2>TECHNICAL SKILLS</h2>
+        <p>
+          <strong>Core: </strong>
+          React Native, Expo, React.js, Next.js, Node.js, JavaScript,
+          TypeScript, Kotlin
+        </p>
+        <p>
+          <strong>State & Data Management: </strong>
+          Redux, Redux-Toolkit, Zustand, Axios, React Query (TanStack Query)
+        </p>
+        <p>
+          <strong>Backend & APIs: </strong>
+          REST APIs, GraphQL, WebSocket
+        </p>
+        <p>
+          <strong>Database: </strong>
+          Firebase, SQLite, PostgreSQL, Supabase
+        </p>
+        <p>
+          <strong>DevOps & Deployment: </strong>
+          Docker, CI/CD Pipelines, Git, Bitbucket, GitLab, App Store & Play
+          STore Deployment
+        </p>
+        <p>
+          <strong>Tools & Platforms: </strong>
+          VS COde, Xcode, Android Studio, Jira, Postman, Figma
+        </p>
+        <p>
+          <strong>Testing: </strong>
+          Jest, React Native Testing Library, Detox
+        </p>
+        <p>
+          <strong>Services & Integrations: </strong>
+          Stripe, Razorpay, Firebase Auth, Cloud Messaging, Push Notifications,
+          Google Vision API
+        </p>
+      </div>
+
+      {/*  MARK:- PAGE-2 */}
+      <div className="page">
+        {/*  MARK:- Projects */}
+        <h2 className="mt-0">PROJECTS</h2>
+        <p>
+          <strong>Learning and Exam preparation app (Iyal)</strong>
+        </p>
+        <ul>
+          <li>
+            Engineered a learning and exam preparation app using Expo, React
+            Native, Zustand, supabase, and Firebase, providing users with a
+            comprehensive platform for educational content and practice exams.
+          </li>
+          <li>
+            Designed and implemented core features including authentication,
+            study material upload, and real-time data synchronization.
+          </li>
+          <li>
+            Developed advanced functionalities such as AI-powered study material
+            generation, text-to-speech (TTS), mock tests, and flashcards.
+          </li>
+          <li>
+            Established CI/CD pipelines and implemented tools such as Docker,
+            Jest, Husky, and ESLint to improve code quality and streamline
+            deployment.
+          </li>
+          <li>
+            Integrated analytics to track user engagement and performance,
+            allowing for data-driven improvements and personalized learning
+            paths.
+          </li>
+        </ul>
+
+        <p>
+          <strong>Children's Storytelling App (Kahani Kids)</strong>
+        </p>
+        <ul>
+          <li>
+            Reached 160K+ downloads by developing an AI-powered storytelling app
+            for children (ages 3+) on the Google Play Store.
+          </li>
+          <li>
+            Led end-to-end development using React Native, Firebase, and backend
+            APIs, ensuring scalability and smooth UX.
+          </li>
+          <li>
+            Built premium subscription features with in-app purchases and
+            ad-free experience.
+          </li>
+        </ul>
+
+        <p>
+          <strong>Tamil Nadu Government App (TN NHIS)</strong>
+        </p>
+        <ul>
+          <li>
+            Built a Tamil Nadu Government mobile app for employees and
+            pensioners to track and apply for claims with 100K+ downloads.
+          </li>
+          <li>
+            Built from scratch using React Native, Redux, firebase and Backend
+            APIs for claims, insurance and hospital listings.
+          </li>
+        </ul>
+
+        <p>
+          <strong>E-Commerce Mobile & Web App (F&F-Bargains)</strong>
+        </p>
+        <ul>
+          <li>
+            Led development and maintenance of web and mobile application,
+            incorporating Firebase authentication, Type-Sense for fast search,
+            Stripe payments Gateway, and Easy Post for delivery tracking.
+          </li>
+          <li>
+            Built real-time features such as live auctions, bidding, comments,
+            customer support, and live chat to enhance user engagement and
+            interaction. Successfully attracted 10,000 new users within two
+            months.
+          </li>
+        </ul>
+
+        {/*  MARK:- Personal Projects & Open Source */}
+        <h2>PERSONAL PROJECTS & OPEN SOURCE</h2>
+        <ul>
+          <li>
+            Published React Native libraries used for developer productivity and
+            app features (react-native-dynamic-shortcut, react-native-pip-mode,
+            toast-react-native etc.)
+          </li>
+          <li>
+            Built a Google Lens-like application using Google Vision API for
+            text detection and QR scanning.
+          </li>
+        </ul>
+
+        {/*  MARK:- Education */}
+        <h2>EDUCATION</h2>
+        <div className="header-justify">
+          <p>
+            <strong>Master of Computer Application</strong>
+            <br />
+            Biju Patnaik University of Technology
+          </p>
+          <p className="place-date-box">
+            (2021–2023)
+            <br />
+            Bhubaneswar, Odisha
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
