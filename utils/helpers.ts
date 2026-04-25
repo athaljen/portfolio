9f8b3f08231371113072a38a59f31fe5fa3b2c4b
Bisworanjan Jena <athaljen@gmail.com>
2026-04-25 22:06:05 +0530

    update animation and added cat sound

---
+++ utils/helpers.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+): T {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null
+  return ((...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => func(...args), delay)
+  }) as T
+}
