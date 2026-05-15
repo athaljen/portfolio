e15570c3c2ff1fbc8b57d45ccf54cbf4588fd8d8
Bisworanjan Jena <athaljen@gmail.com>
2026-05-15 23:23:27 +0530

    add send mail

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -1,10 +1,52 @@
-// app/api/send-mail/route.ts
 import { NextResponse } from 'next/server'
+import { Resend } from 'resend'
+
+import { PERSONAL_INFO } from '@/constants'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: Request) {
-  const data = await req.json()
+  try {
+    const body = await req.json()
 
-  // send email logic here
+    const { name, email, message } = body
 
-  return NextResponse.json({ success: true })
+    if (!name || !email || !message) {
+      return NextResponse.json(
+        { error: 'All fields are required' },
+        { status: 400 },
+      )
+    }
+
+    const { data, error } = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: PERSONAL_INFO.email,
+      subject: `New message from ${name}`,
+      replyTo: email,
+      html: `
+        <h2>New Contact Form Message</h2>
+
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+
+        <p>${message}</p>
+      `,
+    })
+
+    if (error) {
+      return Response.json(
+        { success: false, error: error.message },
+        { status: 400 },
+      )
+    }
+
+    return NextResponse.json({
+      success: true,
+      data,
+      message: 'Message sent successfully',
+    })
+  } catch {
+    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 })
+  }
 }
--- constants/index.ts
+++ constants/index.ts
@@ -9,3 +9,10 @@ export const PERSONAL_LINKS = {
   hireMe:
     'mailto:bisworanjanjena23@gmail.com?subject=Hiring%20Bisworanjan%20Jena&body=Hello%20Bisworanjan%2C%0A%0AI%20am%20interested%20in%20hiring%20you.%20Please%20let%20me%20know%20your%20availability%20for%20a%20discussion.%0A%0AThank%20you.%0A',
 }
+
+export const PERSONAL_INFO = {
+  name: 'Bisworanjan Jena',
+  title: 'Full Stack Developer',
+  email: 'bisworanjanjena23@gmail.com',
+  phone: '+91 6370584925',
+}
