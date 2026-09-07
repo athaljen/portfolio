09c1f7c59b819c7cf801415e2f323d78f9a146be
Bisworanjan Jena <athaljen@gmail.com>
2026-09-07 23:17:52 +0530

    update to static

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next'
 
 import portfolioData from '@/data/portfolioData'
 
+export const dynamic = 'force-static'
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: portfolioData.name,
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.31.115'],
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
 }
 
 export default nextConfig
